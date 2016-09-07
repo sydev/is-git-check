beee8a7b9f3468e85e984495c1fabc3b9dae487a
Dominik Winter <dominik.winter@sydev.de>
2016-09-07 12:17:17 +0200

    added comments

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
   const isThere = require('is-there');
   const path    = require('path');
 
+  /**
+   * Checks whether a directory is a git repository or not
+   * @param  {String}  dir [path to git repository]
+   * @return {Boolean}
+   */
   function isGit(dir) {
     let gitDir = path.join(dir, '.git');
 
